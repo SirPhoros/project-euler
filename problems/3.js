bf21f8cf5008c09096050c8626647b7210b55f93
SirPhoros <cgtorrubia@gmail.com>
2023-07-03 13:25:52 +0100

    Solve problem 3

---
+++ problems/3.js
@@ -0,0 +1,18 @@
+// Problem 3: Largest prime factor
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the given number?
+
+function largestPrimeFactor(number) {
+    let prime = 2,
+      max = 1;
+    while (prime <= number) {
+      if (number % prime == 0) {
+        max = prime;
+        number = number / prime;
+      } else prime++; //Only increment the prime number if the number isn't divisible by it
+    }
+    return max;
+  }
+ console.log(largestPrimeFactor(13195));
